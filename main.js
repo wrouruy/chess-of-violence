c514cdc4930e8eb4df4c8fbc06732d232bf3dea9
wrouruy <161200383+wrouruy@users.noreply.github.com>
2024-12-22 12:25:14 +0200

    Update main.js

--- main.js
+++ main.js
@@ -6,8 +6,8 @@ const ctx = canvas.getContext('2d');
 const pawImg = new Image()
 const bgImg = new Image()
 
-pawImg.src = './img/pawn.png'
-bgImg.src = './img/background.png'
+pawImg.src = '/img/pawn.png'
+bgImg.src = '/img/background.png'
 // const camera = {
 //     x: 0,
 //     y: 0
