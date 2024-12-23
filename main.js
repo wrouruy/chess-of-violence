576ce59e96f970310f64a9e6be8fbc1c518bda1f
wrouruy <161200383+wrouruy@users.noreply.github.com>
2024-12-23 21:31:43 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -5,16 +5,16 @@ const ctx = canvas.getContext('2d');
 
 const pawImg = new Image()
 const bgImg = new Image()
+const gunImg = new Image()
+const bulletImg = new Image()
 
-pawImg.src = './img/pawn.png'
-bgImg.src = './img/background.png'
-// const camera = {
-//     x: 0,
-//     y: 0
-// }
+pawImg.src = './img/pawn.png';
+bgImg.src = './img/background.png';
+gunImg.src = './img/gun.png';
+bulletImg.src = './img/bullet.png';
+
 ctx.font = '25px Silkscreen';
 ctx.strokeStyle = 'white'
-
 
 let player = {
     x: 0,
@@ -26,7 +26,8 @@ let player = {
     moveRight: false,
     moveDown: false,
     moveUp: false,
-    velocity: 2.5
+    velocity: 2.5,
+    isShoot: false
 }
 player.x -= player.width / 2
 player.y -= player.height / 2
@@ -37,7 +38,9 @@ const room = {
     x: -320,
     y: -320
 }
+let allBullet = []
 
+let cursorX, cursorY
 
 document.addEventListener('keydown', function(e){
     if(e.keyCode === 68){player.moveRight = true}
@@ -49,7 +52,7 @@ document.addEventListener('keydown', function(e){
     if(e.key === 'ArrowLeft'){player.moveLeft = true}
     if(e.key === 'ArrowDown'){player.moveDown = true}
     if(e.key === 'ArrowUp'){player.moveUp = true}
-})
+});
 document.addEventListener('keyup', function(e){
     if(e.keyCode === 68){player.moveRight = false}
     if(e.keyCode === 65){player.moveLeft = false}
@@ -60,67 +63,94 @@ document.addEventListener('keyup', function(e){
     if(e.key === 'ArrowLeft'){player.moveLeft = false}
     if(e.key === 'ArrowDown'){player.moveDown = false}
     if(e.key === 'ArrowUp'){player.moveUp = false}
+});
+document.onmousemove = function (e) {
+    const rect = canvas.getBoundingClientRect();
+    cursorX = e.clientX - rect.left - canvas.width / 2;
+    cursorY = e.clientY - rect.top - canvas.height / 2;
+};
+document.addEventListener('click', function(){
+    player.isShoot = true
 })
 
-function draw(){
+function draw() {
     [canvas.width, canvas.height] = [window.innerWidth, window.innerHeight];
-    ctx.translate(canvas.width / 2, canvas.height / 2)
+    ctx.translate(canvas.width / 2, canvas.height / 2);
 
-    ctx.clearRect(0, 0, canvas.width, canvas.height)
-    // for(let i=0;i<Math.floor(room.width/10);i+=2){
-    //     ctx.fillRect((10*i) + room.width, 0, Math.floor(room.width/10), Math.floor(room.width/10))
-    // }
+    // movement player
+    if (player.moveLeft) player.x -= player.velocity;
+    if (player.moveRight) player.x += player.velocity;
+    if (player.moveDown) player.y += player.velocity;
+    if (player.moveUp) player.y -= player.velocity;
 
-
-    // movement for player
-    if(player.moveLeft){ player.x-=player.velocity }
-    if(player.moveRight){ player.x+=player.velocity }
-    if(player.moveDown){ player.y+=player.velocity }
-    if(player.moveUp){ player.y-=player.velocity }
-
-    // check collision, exit to room
-    if(player.x + player.width > room.x + room.width){
+    // collision with wall at room
+    if (player.x + player.width > room.x + room.width) {
         player.moveRight = false;
-        player.x -= player.velocity
-    } 
-    if(player.x < room.x){
+        player.x -= player.velocity;
+    }
+    if (player.x < room.x) {
         player.moveLeft = false;
-        player.x += player.velocity
-    } 
-    if(player.y + player.height > room.y + room.height){
+        player.x += player.velocity;
+    }
+    if (player.y + player.height > room.y + room.height) {
         player.moveUp = false;
-        player.y -= player.velocity
-    } 
-    if(player.y + player.height - 27 < room.y){
+        player.y -= player.velocity;
+    }
+    if (player.y + player.height - 27 < room.y) {
         player.moveDown = false;
-        player.y += player.velocity
-    }
-    if(player.x > room.x && player.x + player.width < room.x + room.width &&
-    player.y > room.y && player.y + player.height < room.y + room.height
-    ){velocity = 0} else {
-        player.velocity = 2.5
+        player.y += player.velocity;
     }
 
-    ctx.fillStyle = '#252525'
-    body.style.backgroundColor = '#252525'
+    ctx.fillStyle = '#252525';
+    ctx.fillRect(-canvas.width / 2, -canvas.height / 2, canvas.width, canvas.height);
 
-    ctx.fillRect((canvas.width / 2) * -1, (canvas.height / 2) * -1, canvas.width, canvas.height)
-
-    ctx.fillStyle = 'white';
-    for (let i = 0; i < room.width / player.width / 2; i++) { 
+    // draw room
+    for (let i = 0; i < room.width / player.width / 2; i++) {
         for (let j = 0; j < room.width / player.width / 2; j++) {
-            ctx.drawImage(bgImg, room.x + (player.width * 2) * j, room.y + (player.width * 2) * i, player.width * 2, player.width * 2);
-            // ctx.drawImage(bgImg, ((room.x + (player.width * 2) * j)-player.x)-player.width / 2, ((room.y + (player.width * 2) * i)-player.y)-player.height / 2, player.width * 2, player.width * 2);
+            ctx.drawImage(
+                bgImg,
+                room.x + player.width * 2 * j,
+                room.y + player.width * 2 * i,
+                player.width * 2,
+                player.width * 2
+            );
         }
-
     }
-    // for (let i = 0; i < room.width / player.width; i++) {
-    //     ctx.fillText(8 - i, room.x - 40, room.y + (player.width) * i + 45);
-    // }
+    if (player.isShoot) {
+        angle = Math.atan2(cursorY - (player.y + 85), cursorX - (player.x + 60));
+    
+        allBullet.push({
+            x: player.x + 60 + Math.cos(angle) * 50,
+            y: player.y + 65 + Math.sin(angle) * 50,
+            angle: angle,
+        });
+    
+        // player.isShoot = false;
+    }
+    for (let i = 0; i < allBullet.length; i++) {
+        const bullet = allBullet[i];
+
+        bullet.x += Math.cos(bullet.angle) * 10;
+        bullet.y += Math.sin(bullet.angle) * 10;
+    
+        ctx.save();
+        ctx.translate(bullet.x, bullet.y);
+        ctx.rotate(bullet.angle);
+        ctx.drawImage(bulletImg, -6, -7.5, 12, 15);
+        ctx.restore();
+    }
     
 
-    ctx.drawImage(pawImg, player.x, player.y, player.width, player.height)
-    // ctx.drawImage(pawImg, 0-player.width / 2, 0-player.height / 2, player.width, player.height)
-    requestAnimationFrame(draw)
+    ctx.save();
+    ctx.translate(player.x + 60, player.y + 85);
+    angle = Math.atan2(cursorY - (player.y + 85), cursorX - (player.x + 60));
+    ctx.rotate(angle);
+    ctx.drawImage(gunImg, -5, -15, 75, 50);
+
+
+    ctx.restore();
+
+    ctx.drawImage(pawImg, player.x, player.y, player.width, player.height);
+    requestAnimationFrame(draw);
 }
 document.onload = draw()
